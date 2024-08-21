2bd33a36eea624e4ca852874baa2f0fbafc96cae
Kumbhani-Juli <julikumbhani5981@gmail.com>
2024-08-21 17:28:13 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<>
-			<Header />
-			<Home />
-			<Footer />
+			<BrowserRouter basename={process.env.PUBLIC_URL}>
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<Header />
+								<Home />
+								<Footer />
+							</>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
